6871fbc301fb161a6276856551fcb3c360a68d4c
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2025-04-10 19:01:06 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,68 @@
+import express, { Request, Response } from "express";
+import {
+  McpServer,
+  ResourceTemplate,
+} from "@modelcontextprotocol/sdk/server/mcp.js";
+import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
+import { z } from "zod";
+
+const server = new McpServer({
+  name: "example-server",
+  version: "1.0.0",
+});
+
+server.resource(
+  "echo",
+  new ResourceTemplate("echo://{message}", { list: undefined }),
+  async (uri, { message }) => ({
+    contents: [
+      {
+        uri: uri.href,
+        text: `Resource echo echo: ${message}`,
+      },
+    ],
+  })
+);
+
+server.tool("echo", { message: z.string() }, async ({ message }) => ({
+  content: [{ type: "text", text: `Tool echo: ${message} ${message}` }],
+}));
+
+server.prompt("echo", { message: z.string() }, ({ message }) => ({
+  messages: [
+    {
+      role: "user",
+      content: {
+        type: "text",
+        text: `Please process this message: ${message}`,
+      },
+    },
+  ],
+}));
+
+const app = express();
+
+// to support multiple simultaneous connections we have a lookup object from
+// sessionId to transport
+const transports: { [sessionId: string]: SSEServerTransport } = {};
+
+app.get("/sse", async (_: Request, res: Response) => {
+  const transport = new SSEServerTransport("/messages", res);
+  transports[transport.sessionId] = transport;
+  res.on("close", () => {
+    delete transports[transport.sessionId];
+  });
+  await server.connect(transport);
+});
+
+app.post("/messages", async (req: Request, res: Response) => {
+  const sessionId = req.query.sessionId as string;
+  const transport = transports[sessionId];
+  if (transport) {
+    await transport.handlePostMessage(req, res);
+  } else {
+    res.status(400).send("No transport found for sessionId");
+  }
+});
+
+app.listen(3001);
